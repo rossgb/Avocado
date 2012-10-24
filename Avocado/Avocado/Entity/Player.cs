7afa67dfbaf6c6d26936f276bc6b8013fee30e7f
leonsenft <leonsenft@gmail.com>
2012-10-24 15:07:57 -0700

    Update Avocado/Avocado/Entity/Player.cs

--- Avocado/Avocado/Entity/Player.cs
+++ Avocado/Avocado/Entity/Player.cs
@@ -17,16 +17,6 @@ namespace Avocado
 			this.health = health;
 		}
 
-		public override void Update(GameTime gameTime)
-		{
-			base.Update(gameTime);
-		}
-
-		public override void Draw(SpriteBatch spriteBatch)
-		{
-			base.Draw(spriteBatch);
-		}
-
 		public void HandleInput(InputState input, int index)
 		{
 			this.Direction = input.CurrentGamePadStates[index].ThumbSticks.Left;
