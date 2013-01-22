bca129e7a5437cea606b6170c4e565307ffe0079
unknown <leons@a10.seng.uvic.ca>
2013-01-21 20:12:03 -0800

    Yummy collision resolution between players.

--- Avocado/Avocado/Game/Collision.cs
+++ Avocado/Avocado/Game/Collision.cs
@@ -1,9 +1,21 @@
-﻿namespace Avocado
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Avocado
 {
 	class Collision
 	{
 		public static void resolve(Player player1, Player player2)
 		{
+
+            Vector2 distance = player1.Position - player2.Position;
+            float overlap = player1.Radius + player2.Radius - distance.Length();
+            double angle = Math.Atan2(distance.Y, distance.X);
+
+            player1.Position.X += (float) (overlap * Math.Cos(angle));
+            player1.Position.Y += (float) (overlap * Math.Sin(angle));
+            player2.Position.X -= (float) (overlap * Math.Cos(angle));
+            player2.Position.Y -= (float) (overlap * Math.Sin(angle));
 		}
 
 		public static void resolve(Player player, Enemy enemy)
