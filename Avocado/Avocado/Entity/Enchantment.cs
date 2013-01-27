c7f11fed9164e60b63d617628f9e4db9fe8cf835
leonsenft <leonsenft@gmail.com>
2013-01-27 12:49:25 -0800

    Update Avocado/Avocado/Entity/Enchantment.cs

--- Avocado/Avocado/Entity/Enchantment.cs
+++ Avocado/Avocado/Entity/Enchantment.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Avocado
 {
+	
 	class Enchantment : Item
 	{
 		public Enchantment(Texture2D texture, Vector2 position, float speed, int radius) :
