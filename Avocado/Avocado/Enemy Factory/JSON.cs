d4f4a2265e0dd3e79039b4601df44e66b710bb1c
Dylan Gedig <dylangedig@gmail.com>
2013-02-11 21:39:06 -0800

    Set up files for JSON parser, pre-work commit

---
+++ Avocado/Avocado/Enemy Factory/JSON.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Text;
+
+namespace Avocado
+{
+	/*Dyl's big Class. Reads the JSON strings and puts the waves into an ArrayList with the properties of each wave located in a Hashtable.
+     This allows us to have optional parameters for each wave, as well as allowing each wave to have multiple enemies.
+     
+     Sample JSON string:*/
+	public class JSON
+	{
+		
+	}
+}
+
